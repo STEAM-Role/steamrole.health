73279ef4b4d23f1f69c70730ba973fa79b82bad1
Diego Resendez <aion.chrno@gmail.com>
2018-06-26 23:48:27 -0500

    Initialize Project

---
+++ server.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const Queue = require('bull');
+const healthQueue = new Queue('health-server');
+const request = require('request-promise');
+const checks = require('./lib/healthcheck.json');
+const notify = require('./lib/slack-notify');
+const $P = require('bluebird');
+
+healthQueue.process(async (job, done) => {
+  console.log('Executed Queue');
+  const promises = checks.map(task => healthcheck(task));
+  await $P.all(promises);
+  done();
+});
+
+function healthcheck(task) {
+  return new $P(resolve => {
+    request(task.url)
+      .then(() => console.log('Works!!'))
+      .catch(() => notification(task))
+      .finally(() => resolve(true));
+  });
+}
+
+function notification(task) {
+  notify({
+    'attachments': [{
+      'fallback': `Health fails on ${task.env} with ${task.url}`,
+      'color': '#DC143C',
+      'pretext': `Health fails on ${task.env} with ${task.url}`,
+      'author_name': 'Healthcheck API',
+      'title': `API on ${task.env} is down`,
+      'text': task.message,
+      'footer': 'STEAM Role API',
+      'footer_icon': 'https://platform.slack-edge.com/img/default_application_icon.png',
+      'ts': Math.round((new Date()).getTime() / 1000)
+    }]
+  });
+}
+healthQueue.add({}, {
+  repeat: {
+    cron: '* * * * *'
+  }
+});
